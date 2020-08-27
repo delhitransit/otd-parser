d532d7f44e2e678d77932086b723b30090c8b5f7
singaltanmay <tanmaysingal@icloud.com>
2020-08-27 17:04:46 +0530

    Added findBy methods to RouteRepository

--- src/main/java/com/delhitransit/otdparser/repository/RouteRepository.java
+++ src/main/java/com/delhitransit/otdparser/repository/RouteRepository.java
@@ -8,7 +8,19 @@ import com.delhitransit.otdparser.model.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
+
+    List<Route> findAllByRouteId(long routeId);
+
+    List<Route> findAllByShortName(String name);
+
+    List<Route> findAllByLongName(String name);
+
+    List<Route> findAllByType(int type);
+
+    List<Route> findAllByAgencyId(String agencyId);
 
 }
