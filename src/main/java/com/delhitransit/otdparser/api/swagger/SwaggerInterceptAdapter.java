342ae451b1229423c5f359dbe9e0983356aea326
Tanmay Singal <tanmaysingal@icloud.com>
2020-10-02 17:37:28 +0530

    Removed some whitespaces

--- src/main/java/com/delhitransit/otdparser/api/swagger/SwaggerInterceptAdapter.java
+++ src/main/java/com/delhitransit/otdparser/api/swagger/SwaggerInterceptAdapter.java
@@ -13,20 +13,16 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class SwaggerInterceptAdapter extends HandlerInterceptorAdapter {
 
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
         String req = request.getRequestURI();
-
         if ((req == null) || req.isEmpty() || req.equals("/") || req.equals("/v1") || req
                 .equals("/v1/")) {
             response.sendRedirect("/swagger-ui.html");
             return false;
         }
-
         return super.preHandle(request, response, handler);
     }
-
 
 }
